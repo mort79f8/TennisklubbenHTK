7acd9680f41b46bbc67adef47e34fe3f519a5e1c
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-10 14:56:02 +0100

    started on MemberGUI

--- TennisklubbenHTK.GUI/MemberGui.xaml.cs
+++ TennisklubbenHTK.GUI/MemberGui.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +12,46 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TennisklubbenHTK.BIZ;
+using TennisklubbenHTK.Entities;
 
 namespace TennisklubbenHTK.GUI
 {
     /// <summary>
     /// Interaction logic for MemberGui.xaml
     /// </summary>
-    public partial class MemberGui : Window
+    public partial class MemberGui : Window, INotifyPropertyChanged
     {
+        private Member selectedMember;
+        public List<Member> ListOfMembers { get; set; }
+        public Member SelectedMember 
+        {
+            get => selectedMember;
+            set
+            {
+                selectedMember = value;
+                OnPropertyChanged(nameof(SelectedMember));
+            }
+        }
+
+        Biz biz;
         public MemberGui()
         {
             InitializeComponent();
+            DataContext = this;
+            biz = new Biz();
+            ListOfMembers = biz.GetListOfMembers();
         }
+
+        private void ListOfMembersDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedMember = ListOfMembersDataGrid.SelectedItem as Member;            
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
