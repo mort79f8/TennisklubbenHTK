de91ecaf6c380f486238d78f0e124cfca34c3d5e
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-10 14:17:30 +0100

    main page created might need more polish

--- TennisklubbenHTK.GUI/Members.xaml.cs
+++ TennisklubbenHTK.GUI/Members.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TennisklubbenHTK.BIZ;
 
 namespace TennisklubbenHTK.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Members.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Members : Window
     {
-        public MainWindow()
+        public Members()
         {
             InitializeComponent();
-            
         }
     }
 }
