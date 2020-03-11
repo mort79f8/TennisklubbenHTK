1ff26577067de4240620d169591dc13c402c4e6d
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-11 14:00:55 +0100

    deleted ReservationMember and Simplyfied Reservation

--- TennisklubbenHTK.Entities/Reservation.cs
+++ TennisklubbenHTK.Entities/Reservation.cs
@@ -11,6 +11,9 @@ namespace TennisklubbenHTK.Entities
         public int ReservationId { get; set; }
         public string Time { get; set; }
         public int Court { get; set; }
+        public Member MemberOne { get; set; }
+        public Member MemberTwo { get; set; }
+
 
     }
 }
