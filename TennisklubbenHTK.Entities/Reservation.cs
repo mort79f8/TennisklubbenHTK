583769ba8062cfd7ce3c133c481b3d5de05d4afa
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-11 13:20:12 +0100

    reservationmember

---
+++ TennisklubbenHTK.Entities/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisklubbenHTK.Entities
+{
+    public class Reservation
+    {
+        public int ReservationId { get; set; }
+        public string Time { get; set; }
+        public int Court { get; set; }
+
+    }
+}
