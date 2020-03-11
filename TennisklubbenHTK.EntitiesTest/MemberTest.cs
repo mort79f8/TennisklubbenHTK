01f95ce3d8a80f33c593da0d8aa506d5961da50a
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-11 14:55:31 +0100

    Added a project for unit test

---
+++ TennisklubbenHTK.EntitiesTest/MemberTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisklubbenHTK.Entities;
+
+namespace TennisklubbenHTK.EntitiesTest
+{
+    [TestClass]
+    public class MemberTest
+    {
+        [TestMethod]
+        public void ValidationFirstName_TooShortReturnsFalse()
+        {
+            // Arrange
+            string name = "n";
+
+            // Act
+            var isFirstnameValid = Member.FirstnameValidation(name);
+
+            // Assert
+            Assert.IsFalse(isFirstnameValid.isValid);
+        }
+
+        [TestMethod]
+        public void ValidationFirstName_NameContainDigitsReturnsFalse()
+        {
+            // Arrange            
+            string name = "n123";
+
+            // Act
+            var isFirstnameValid = Member.FirstnameValidation(name);
+
+            // Assert
+            Assert.IsFalse(isFirstnameValid.isValid);
+        }
+
+        [TestMethod]
+        public void ValidationFirstName_NameToolongReturnsFalse()
+        {
+            // Arrange            
+            // name is 16 char. long
+            string name = "qazxswedcvfrtgbn";
+
+            // Act
+            var isFirstnameValid = Member.FirstnameValidation(name);
+
+            // Assert
+            Assert.IsFalse(isFirstnameValid.isValid);
+        }
+    }
+}
---
+++ TennisklubbenHTK.EntitiesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisklubbenHTK.EntitiesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisklubbenHTK.EntitiesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e510e2b7-6dbf-4c65-b41c-72407c8ccf78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
