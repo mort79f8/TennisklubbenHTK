3f73e3b653e7b985b29bf8dea042f8861809ed22
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-03-16 09:40:01 +0100

    being stupid and start reservations on master

---
+++ TennisklubbenHTK.DAL/ReservationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisklubbenHTK.Entities;
+
+namespace TennisklubbenHTK.DAL
+{
+    class ReservationRepository
+    {
+        public ReservationRepository()
+        {
+
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("TennisklubbenHTKDB")))
+            {
+                return connection.Query<Reservation>("SELECT * FROM Reservation INNER JOIN Court ON Reservation.Court = Court.CourtId").ToList();
+            }
+        }
+    }
+}
--- TennisklubbenHTK.Entities/Reservation.cs
+++ TennisklubbenHTK.Entities/Reservation.cs
@@ -10,7 +10,7 @@ namespace TennisklubbenHTK.Entities
     {
         public int ReservationId { get; set; }
         public string Time { get; set; }
-        public int Court { get; set; }
+        public Court Court { get; set; }
         public int MemberOne { get; set; }
         public int MemberTwo { get; set; }
 
